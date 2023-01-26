92a27d24758e586eae23591f904d0beed5bb9ea7
LianLambert <lian1lambert@gmail.com>
2023-01-26 17:15:51 -0500

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI scoreText;
+    public GameObject winTextObject;
+
+    private Rigidbody rb;
+    private int score;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+
+        SetScoreText();
+        winTextObject.SetActive(false);
+    }
+
+
+    void OnMove(InputValue movementZValue)
+    {
+        Vector2 movementVector = movementZValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        if (score == 10)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            score += 1;
+
+            SetScoreText();
+        }
+    }
+
+}
+
